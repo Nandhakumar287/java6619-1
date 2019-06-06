4b4aa4d296dcb592bfadab896a3013a660d3cff7
Nandha kumar S <mailstonandhakumar@gmail.com>
2019-06-06 09:39:29 +0530

    Update and rename README.md to SortCheck.java

---
+++ SortCheck.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+  public class SortCheck{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      int l=0;
+      //System.out.println("Enter N value:");
+      int N=sc.nextInt();
+      int a[]=new int[N];
+      //System.out.println("Enter the Array element one by one...");
+        for(int i=0;i<N;i++)
+        {
+           a[i]=sc.nextInt();
+        }
+        for(int i=0;i<N-1;i++)
+        {
+          if(a[i]<a[i+1])
+          {
+            status=0;
+          }
+          else
+          {
+          status=1;
+          }
+        }
+        if(status==1)
+        {
+        System.out.println("No");
+        }
+        else
+        {
+        System.out.println("Yes");
+        }
+     }
+   }  
