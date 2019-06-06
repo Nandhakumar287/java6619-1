7be2619aba2cfde4f3c471cc724625fc2425b00a
Nandha kumar S <mailstonandhakumar@gmail.com>
2019-06-06 09:40:04 +0530

    Update SortCheck.java

--- SortCheck.java
+++ SortCheck.java
@@ -3,7 +3,7 @@ import java.util.*;
   public class SortCheck{
     public static void main(String args[]){
       Scanner sc=new Scanner(System.in);
-      int l=0;
+      int l=0,status=0;
       //System.out.println("Enter N value:");
       int N=sc.nextInt();
       int a[]=new int[N];
